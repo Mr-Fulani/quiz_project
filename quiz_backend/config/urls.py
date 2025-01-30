0ac6353fee56e4b15d182a37b01564a60dd50715
Anvar <anvar.sharipov.1986@gmail.com>
2025-01-31 00:08:04 +0300

    Исправил API инетерфейс

--- quiz_backend/config/urls.py
+++ quiz_backend/config/urls.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Quiz API",
+        default_version='v1',
+        description="API documentation for Quiz project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@quiz.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +28,9 @@ urlpatterns = [
     path('api/', include('platforms.urls')),
     path('api/', include('feedback.urls')),
     path('api/webhooks/', include('webhooks.urls')),
+    
+    # Swagger URLs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
