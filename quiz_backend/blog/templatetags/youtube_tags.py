16eee58e067a7c7f1e9334f66115d22458aead21
Anvar <anvar.sharipov.1986@gmail.com>
2025-03-30 21:37:22 +0300

    Исправил паузы для видео из ютуб

--- quiz_backend/blog/templatetags/youtube_tags.py
+++ quiz_backend/blog/templatetags/youtube_tags.py
@@ -6,19 +6,24 @@ register = template.Library()
 
 @register.filter
 def youtube_embed_url(value):
-    """Преобразует любую ссылку YouTube (включая Shorts) в embed-формат"""
+    """Преобразует любую ссылку YouTube (включая Shorts) в embed-формат с enablejsapi=1"""
     if not value:
         return value
+
     # Обычные видео: youtube.com/watch?v=VIDEO_ID или youtu.be/VIDEO_ID
     match_standard = re.search(r'(?:youtube\.com/watch\?v=|youtu\.be/)([a-zA-Z0-9_-]{11})', value)
     # Shorts: youtube.com/shorts/VIDEO_ID
     match_shorts = re.search(r'youtube\.com/shorts/([a-zA-Z0-9_-]{11})', value)
+    # Embed: youtube.com/embed/VIDEO_ID (возможно, с параметрами)
+    match_embed = re.search(r'youtube\.com/embed/([a-zA-Z0-9_-]{11})', value)
 
     if match_standard:
         video_id = match_standard.group(1)
     elif match_shorts:
         video_id = match_shorts.group(1)
+    elif match_embed:
+        video_id = match_embed.group(1)
     else:
         return value  # Если не распознано, возвращаем как есть
 
-    return f"https://www.youtube.com/embed/{video_id}"
+    return f"https://www.youtube.com/embed/{video_id}?enablejsapi=1"
