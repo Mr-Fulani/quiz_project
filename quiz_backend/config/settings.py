81925b08d7b268cde20fbebeb11e5bf8ca5becd3
Anvar <anvar.sharipov.1986@gmail.com>
2025-06-05 01:41:56 +0300

    Локализация 1.0

--- quiz_backend/config/settings.py
+++ quiz_backend/config/settings.py
@@ -118,6 +118,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -194,7 +195,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGES = (
+    ('ru', 'Русский'),
+    ('en', 'English'),
+)
+
+# LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-En'
+
+LOCALE_PATHS = [
+    BASE_DIR / 'blog' / 'locale',
+]
 
 TIME_ZONE = 'UTC'
 
