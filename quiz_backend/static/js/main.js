1ae86dc4dd21e4280985906b0d6cc6bafe150f60
Anvar <anvar.sharipov.1986@gmail.com>
2025-07-08 21:45:46 +0300

    Устранил проблему карточки общей статистики 1.0

--- quiz_backend/static/js/main.js
+++ quiz_backend/static/js/main.js
@@ -136,25 +136,4 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-
-    /**
-     * Навигация по страницам через ссылки.
-     */
-    const navigationLinks = document.querySelectorAll('[data-nav-link]');
-    const pages = document.querySelectorAll('[data-page]');
-
-    for (let i = 0; i < navigationLinks.length; i++) {
-        navigationLinks[i].addEventListener('click', function () {
-            for (let j = 0; j < pages.length; j++) {
-                if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
-                    pages[j].classList.add('active');
-                    navigationLinks[j].classList.add('active');
-                    window.scrollTo(0, 0);
-                } else {
-                    pages[j].classList.remove('active');
-                    navigationLinks[j].classList.remove('active');
-                }
-            }
-        });
-    }
 });
