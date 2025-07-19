1340b69af3f36458c569855faa331f1cacb9c26c
Anvar <anvar.sharipov.1986@gmail.com>
2025-07-19 15:41:51 +0300

    Добавлены иконки для всех тем и настроена админка с выпадающим меню
    - Создано 136 иконок (77 PNG + 59 SVG) для 58 тем
    - Настроена админка с выпадающим меню, отсортированным по алфавиту
    - Добавлены команды для создания и управления иконками:
      - create_default_icons.py - создание SVG иконок
      - create_png_icons.py - создание PNG иконок
      - fix_icon_mapping.py - исправление сопоставления иконок
      - download_png_icons.py - скачивание PNG иконок
    - Все темы теперь имеют иконки (100% покрытие)
    - Красивые PNG иконки с цветовым кодированием по категориям

--- quiz_backend/topics/admin.py
+++ quiz_backend/topics/admin.py
@@ -1,32 +1,46 @@
+import os
+from django import forms
 from django.contrib import admin
-from .models import Topic, Subtopic
+from django.conf import settings
+from .models import Topic
 
-@admin.register(Topic)
+class TopicAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        icons_dir = os.path.join(settings.BASE_DIR, 'blog', 'static', 'blog', 'images', 'icons')
+        choices = [('', '--- Выберите иконку ---')]
+        
+        if os.path.exists(icons_dir):
+            icon_files = []
+            for filename in os.listdir(icons_dir):
+                if filename.endswith(('.png', '.jpg', '.jpeg', '.svg')):
+                    icon_files.append(filename)
+            
+            # Сортируем файлы по алфавиту
+            icon_files.sort()
+            
+            for filename in icon_files:
+                icon_path = f'/static/blog/images/icons/{filename}'
+                # Убираем расширение и дефисы для отображения
+                display_name = filename.replace('-icon.png', '').replace('-icon.svg', '').replace('-', ' ').title()
+                choices.append((icon_path, f"{display_name} ({filename})"))
+        
+        self.fields['icon'] = forms.ChoiceField(
+            choices=choices, 
+            required=False, 
+            help_text='Выберите иконку для темы'
+        )
+
 class TopicAdmin(admin.ModelAdmin):
-    """
-    Админка для управления темами
-    """
-    list_display = ('id', 'name', 'description')
-    search_fields = ('name', 'description')
-    ordering = ('name',)
+    form = TopicAdminForm
+    list_display = ['name', 'icon_preview', 'description']
+    search_fields = ['name', 'description']
+    
+    def icon_preview(self, obj):
+        if obj.icon:
+            return f'<img src="{obj.icon}" alt="{obj.name}" style="width: 32px; height: 32px; object-fit: contain;">'
+        return 'Нет иконки'
+    icon_preview.short_description = 'Иконка'
+    icon_preview.allow_tags = True
 
-    def get_subtopics_count(self, obj):
-        """Получить количество подтем"""
-        return obj.subtopics.count()
-    get_subtopics_count.short_description = 'Количество подтем'
-
-@admin.register(Subtopic)
-class SubtopicAdmin(admin.ModelAdmin):
-    """
-    Админка для управления подтемами
-    """
-    list_display = ('id', 'name', 'topic')
-    list_filter = ('topic',)
-    search_fields = ('name', 'topic__name')
-    raw_id_fields = ('topic',)
-    ordering = ('topic', 'name')
-
-    def get_tasks_count(self, obj):
-        """Получить количество задач в подтеме"""
-        return obj.tasks.count()
-    get_tasks_count.short_description = 'Количество задач'
+admin.site.register(Topic, TopicAdmin)
