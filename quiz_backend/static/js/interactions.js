bcaa025c3c71431a0ffed6c07833e6afb7b9825e
Anvar <anvar.sharipov.1986@gmail.com>
2025-07-28 15:25:27 +0300

    Исправлен VK share URL - используем оригинальный URL поста вместо share URL

--- quiz_backend/static/js/interactions.js
+++ quiz_backend/static/js/interactions.js
@@ -338,7 +338,7 @@ class ContentInteractions {
         
         const shareUrls = {
             telegram: `https://t.me/share/url?url=${encodedUrl}&text=${text}`,
-            vk: `https://vk.com/share.php?url=${encodedUrl}`,
+            vk: `https://vk.com/share.php?url=${encodeURIComponent(url)}`, // Используем оригинальный URL для VK
             facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
             twitter: `https://twitter.com/intent/tweet?url=${encodedUrl}&text=${text}`,
             instagram: `https://www.instagram.com/`,
