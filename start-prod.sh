591b2cb16bec9b481b8fe14363c878d295a868d1
Anvar <anvar.sharipov.1986@gmail.com>
2025-09-06 16:22:38 +0300

    Fix: Исправлена проблема с гонкой сертификатов SSL и обновлена конфигурация Nginx для продакшена.

--- start-prod.sh
+++ start-prod.sh
@@ -6,7 +6,30 @@ echo "🌐 Запуск продакшена..."
 # Устанавливаем переменную окружения для продакшен конфигурации
 export NGINX_DOCKERFILE=Dockerfile.prod
 
-# Запускаем контейнеры
-docker-compose down && docker-compose up --build
+# Определяем домены из nginx-prod.conf (для Certbot)
+DOMAINS=$(grep -E 'server_name\s+' nginx/nginx-prod.conf | awk '{for(i=2;i<=NF;i++) print $i}' | tr -d ";" | tr -s ' ' ',' | sed 's/,$//g')
+EMAIL="fulani.dev@gmail.com" # Замените на реальный email
+
+echo "🔌 Остановка и удаление существующих контейнеров..."
+docker compose down
+
+echo "⏳ Запуск Certbot для получения первоначальных сертификатов..."
+docker compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    --staging \
+    --agree-tos \
+    --non-interactive \
+    --email $EMAIL \
+    -d $(echo $DOMAINS | tr ',' ' -d ')"
+certbot
+
+echo "⌛ Ожидание генерации сертификатов..."
+until [ -f "./certbot/conf/live/$(echo $DOMAINS | cut -d',' -f1)/fullchain.pem" ]; do
+  echo "Ожидание Certbot..."
+  sleep 5
+done
+
+echo "✅ Сертификаты сгенерированы! Запуск всех служб..."
+docker compose up -d --build
 
 echo "✅ Продакшен запущен!"
