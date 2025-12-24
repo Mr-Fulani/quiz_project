caae1c8aa83564c1537c2a07caf11ac3bbc63b20
Anvar <anvar.sharipov.1986@gmail.com>
2025-12-24 06:39:25 +0300

    fix: исправление зависимости миграции 0006
    - Изменена зависимость с 0004 на 0003 (более стабильная миграция)
    - Решена проблема запуска Django на продакшене

--- quiz_backend/webhooks/migrations/0006_add_russian_webhook_type.py
+++ quiz_backend/webhooks/migrations/0006_add_russian_webhook_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("webhooks", "0004_add_browser_automation_fields"),
+        ("webhooks", "0003_globalwebhooklink"),
     ]
 
     operations = [
