9162e3223cdb8c4906ffc17a3c8d9019ff12bbbc
Anvar <anvar.sharipov.1986@gmail.com>
2026-05-18 02:02:26 +0300

    Fix topic status import and finalize translation indexes

--- quiz_backend/topics/views.py
+++ quiz_backend/topics/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Topic, Subtopic
 from .serializers import TopicSerializer, SubtopicSerializer, TopicMiniAppSerializer, SubtopicWithTasksSerializer
